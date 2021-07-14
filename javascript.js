9792981fa039707c7c22ab85d1d344286fef4803
marlingr7 <marlin.gu7@gmail.com>
2021-07-14 18:26:50 -0500

    final

--- javascript.js
+++ javascript.js
@@ -1,3 +1,62 @@
+let btn = document.getElementById("numbers");
+let result = document.getElementById("result");
+let backResHalf = document.getElementById("back-res-top-half");
+let backResTop = document.getElementById("back-res-top");
+let backResMedium = document.getElementById("back-res-medium");
+let backResBottom = document.getElementById("back-res-bottom");
+let gif = document.getElementById("gif");
+let btnDelete = document.getElementById("btn-delete");
+let mode = document.getElementById("mode");
+
+btn.addEventListener(
+  "click",
+  (e) => {
+    gif.className = "hidden";
+    let element = e.target;
+
+    switch (element.textContent) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+      case ".":
+        result.textContent = result.textContent + element.textContent;
+        break;
+      case "+":
+      case "-":
+      case "(":
+      case ")":
+      case "%":
+      case "√":
+      case "±":
+        result.textContent =
+          result.textContent + " " + element.textContent + " ";
+        break;
+      case "×":
+        result.textContent = result.textContent + " " + "*" + " ";
+        break;
+      case "÷":
+        result.textContent = result.textContent + " " + "/" + " ";
+        break;
+      case "=":
+        btnEqual();
+        break;
+      case "C":
+        btnC();
+        break;
+      default:
+        break;
+    }
+  },
+  false
+);
+
 function solveCalc() {
   let res = [];
   switch (operation) {
@@ -17,262 +76,116 @@ function solveCalc() {
   return res;
 }
 
-function removeItemFromArr( arr, item ) {
-  return arr.filter( function( e ) {
-      return e !== item;
-  } );
-};
+function removeItemFromArr(arr, item) {
+  return arr.filter(function (e) {
+    return e !== item;
+  });
+}
 
-let btnC = document.getElementById("btn-C");
-let btnDelete = document.getElementById("btn-delete");
-let btnPercent = document.getElementById("btn-percent");
-let btnInicialPar = document.getElementById("btn-initial-par");
-let btnFinalPar = document.getElementById("btn-final-par");
-let btnSquare = document.getElementById("btn-square");
-let btnPlusMinus = document.getElementById("btn-plus-minus");
-let btnPlus = document.getElementById("btn-plus");
-let btnMinus = document.getElementById("btn-minus");
-let btnDot = document.getElementById("btn-dot");
-let btnMult = document.getElementById("btn-mult");
-let btnDiv = document.getElementById("btn-div");
-let btnEqual = document.getElementById("btn-equal");
-let btnCero = document.getElementById("btn-0");
-let btnUne = document.getElementById("btn-1");
-let btnTwo = document.getElementById("btn-2");
-let btnThree = document.getElementById("btn-3");
-let btnFour = document.getElementById("btn-4");
-let btnFive = document.getElementById("btn-5");
-let btnSix = document.getElementById("btn-6");
-let btnSeven = document.getElementById("btn-7");
-let btnEight = document.getElementById("btn-8");
-let btnNine = document.getElementById("btn-9");
+function btnEqual() {
+  backResHalf.textContent = backResTop.textContent;
+  backResTop.textContent = backResMedium.textContent;
+  backResMedium.textContent = backResBottom.textContent;
+  backResBottom.textContent = result.textContent;
 
-let result = document.getElementById("result");
-let backResHalf = document.getElementById("back-res-top-half");
-let backResTop = document.getElementById("back-res-top");
-let backResMedium = document.getElementById("back-res-medium");
-let backResBottom = document.getElementById("back-res-bottom");
-let gif = document.getElementById("gif");
+  let separator = result.textContent.split(" ");
+  let sep = removeItemFromArr(separator, "");
 
-let mode = document.getElementById("mode");
+  let res = [];
+  let indexSeparator = [];
+  let indexDelete = [];
 
-let operator = [];
-let operation = [];
+  //findPar(sep);
 
-let btnNumbers = [
-  {
-    btnName: btnCero,
-    btnValue: "0",
-  },
-  {
-    btnName: btnUne,
-    btnValue: "1",
-  },
-  {
-    btnName: btnTwo,
-    btnValue: "2",
-  },
-  {
-    btnName: btnThree,
-    btnValue: "3",
-  },
-  {
-    btnName: btnFour,
-    btnValue: "4",
-  },
-  {
-    btnName: btnFive,
-    btnValue: "5",
-  },
-  {
-    btnName: btnSix,
-    btnValue: "6",
-  },
-  {
-    btnName: btnSeven,
-    btnValue: "7",
-  },
-  {
-    btnName: btnEight,
-    btnValue: "8",
-  },
-  {
-    btnName: btnNine,
-    btnValue: "9",
-  },
-  {
-    btnName: btnDot,
-    btnValue: ".",
-  },
-];
+  for (let i = 0; i < sep.length; i++) {
+    switch (sep[i]) {
+      case "%":
+        operation = "%";
+        operator = sep[i - 1];
+        res.push(solveCalc());
+        indexSeparator.push(i);
+        indexDelete.push(i - 1);
+        break;
+      case "±":
+        operation = "±";
+        if (sep[i + 1] == "-") {
+          operator = -sep[i + 2];
+          indexDelete.push(i + 1);
+          indexDelete.push(i + 2);
+        } else {
+          operator = sep[i + 1];
+          indexDelete.push(i + 1);
+        }
+        res.push(solveCalc());
+        indexSeparator.push(i);
 
-let btnOperations = [
-  {
-    btnName: btnPlus,
-    btnValue: "+",
-  },
-  {
-    btnName: btnMinus,
-    btnValue: "-",
-  },
-  {
-    btnName: btnMult,
-    btnValue: "*",
-  },
-  {
-    btnName: btnDiv,
-    btnValue: "/",
-  },
-  {
-    btnName: btnInicialPar,
-    btnValue: "(",
-  },
-  {
-    btnName: btnFinalPar,
-    btnValue: ")",
-  },
-  {
-    btnName: btnPercent,
-    btnValue: "%",
-  },
-  {
-    btnName: btnSquare,
-    btnValue: "√",
-  },
-  {
-    btnName: btnPlusMinus,
-    btnValue: "±",
-  },
-];
-
-btnNumbers.forEach((item) => {
-  item.btnName.addEventListener(
-    "click",
-    () => {
-      result.textContent = result.textContent + item.btnValue;
-    },
-    false
-  );
-});
-
-btnOperations.forEach((item) => {
-  item.btnName.addEventListener(
-    "click",
-    () => {
-      result.textContent = result.textContent + " " + item.btnValue + " ";
-    },
-    false
-  );
-});
-
-btnEqual.addEventListener(
-  "click",
-  () => {
-    backResHalf.textContent = backResTop.textContent;
-    backResTop.textContent = backResMedium.textContent;
-    backResMedium.textContent = backResBottom.textContent;
-    backResBottom.textContent = result.textContent;
-
-    let separator = result.textContent.split(" ");
-    let sep = removeItemFromArr(separator, "");
-
-    let res = [];
-    let indexSeparator = [];
-    let indexDelete = [];
-
-    for (let i = 0; i < sep.length; i++) {
-      switch (sep[i]) {
-        case "%":
-          operation= "%";
-          operator= sep[i - 1];
-          res.push (solveCalc());
-          indexSeparator.push(i);
-          indexDelete.push(i - 1);
-          break;
-        case "±":
-          operation = "±";
-          if (sep[i + 1] == "-") {
-            operator = -sep[i + 2];
-            indexDelete.push(i+1);
-            indexDelete.push(i+2);
-          } else {
-            operator = sep[i + 1];
-            indexDelete.push(i+1);
-          }
-          res.push(solveCalc());
-          indexSeparator.push(i);
-          
-          break;
-        case "√":
-          operation = "√";
-          if (sep[i + 1] == "-") {
-            result.textContent = "NaN";
-          } else {
-            operator = sep[i + 1];
-            indexDelete.push(i+1);
-          }
-          res.push(solveCalc());
-          indexSeparator.push(i);
-          break;
-        default:
-          break;
-      }
+        break;
+      case "√":
+        operation = "√";
+        if (sep[i + 1] == "-") {
+          result.textContent = "NaN";
+        } else {
+          operator = sep[i + 1];
+          indexDelete.push(i + 1);
+        }
+        res.push(solveCalc());
+        indexSeparator.push(i);
+        break;
+      default:
+        break;
     }
+  }
 
-    for (let i = 0; i < indexSeparator.length; i ++){
-      sep[indexSeparator[i]] = res[i];
-    }
+  for (let i = 0; i < indexSeparator.length; i++) {
+    sep[indexSeparator[i]] = res[i];
+  }
 
-    let count = 0;
+  let count = 0;
 
-    for (let i = 0; i < indexDelete.length; i ++){
-      sep.splice(indexDelete[i]-count, 1);
-      count += 1;
-    }
+  for (let i = 0; i < indexDelete.length; i++) {
+    sep.splice(indexDelete[i] - count, 1);
+    count += 1;
+  }
 
-    sumValue = sep[0];
+  sumValue = sep[0];
 
-    for (let i = 1; i < sep.length; i++){
-      sumValue += " " + sep[i];
-    }
+  for (let i = 1; i < sep.length; i++) {
+    sumValue += " " + sep[i];
+  }
 
-    result.textContent = sumValue;
-    result.textContent = eval(result.textContent);
-    if (result.textContent == "NaN" || result.textContent == "Infinity") {
-      result.textContent = "";
-      backResTop.textContent = "";
-      backResMedium.textContent = "";
-      backResBottom.textContent = "";
-      gif.className = "show";
-    }
-  },
-  false
-);
-
-btnC.addEventListener(
-  "click",
-  () => {
-    operator = [];
-    operation = [];
+  result.textContent = sumValue;
+  result.textContent = eval(result.textContent);
+  if (
+    result.textContent == "NaN" ||
+    result.textContent == "Infinity" ||
+    result.textContent == "-Infinity"
+  ) {
     result.textContent = "";
-    backResBottom.textContent = "";
-    backResMedium.textContent = "";
+    backResHalf.textContent = "";
     backResTop.textContent = "";
-    gif.className = "hidden";
-  },
-  false
-);
+    backResMedium.textContent = "";
+    backResBottom.textContent = "";
+    gif.className = "show";
+  }
+}
 
-btnDelete.addEventListener(
-  "click",
-  () => {
-    result.textContent = result.textContent.slice(
-      0,
-      result.textContent.length - 1
-    );
-  },
-  false
-);
+function btnC() {
+  operator = [];
+  operation = [];
+  result.textContent = "";
+  backResHalf.textContent = "";
+  backResBottom.textContent = "";
+  backResMedium.textContent = "";
+  backResTop.textContent = "";
+  gif.className = "hidden";
+}
+
+btnDelete.addEventListener("click", () => {
+  result.textContent = result.textContent.slice(
+    0,
+    result.textContent.length - 1
+  );
+});
 
 gif.addEventListener(
   "click",
